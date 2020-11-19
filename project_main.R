f599542e1a2184af58e76f1f17762bf6e9f343b5
Nicola Brazzale <brazzalenicola@gmail.com>
2020-11-19 18:07:52 +0200

    Some histograms plot

--- project_main.R
+++ project_main.R
@@ -1,2 +1,17 @@
+library(ggplot2)
+
 heart <- read.csv(file = 'heart_failure_clinical_records_dataset.csv')
 head(heart)
+
+ggplot(heart, aes(x=age)) + geom_histogram(aes(fill=as.character(sex)), bins = 30) + labs(fill = "Sex")
+
+ggplot(heart, aes(x=age)) + geom_histogram(aes(fill=as.character(DEATH_EVENT)), bins = 30) + labs(fill = "Death")
+
+ggplot(heart, aes(x=creatinine_phosphokinase)) + geom_histogram(aes(fill=as.character(DEATH_EVENT)), bins = 30) + labs(fill = "Death")
+
+ggplot(heart, aes(x=ejection_fraction)) + geom_histogram(aes(fill=as.character(DEATH_EVENT)), bins = 30) + labs(fill = "Death")
+
+ggplot(heart, aes(x=platelets)) + geom_histogram(aes(fill=as.character(DEATH_EVENT)), bins = 30) + labs(fill = "Death")
+
+ggplot(heart, aes(x=serum_creatinine)) + geom_histogram(aes(fill=as.character(DEATH_EVENT)), bins = 30) + labs(fill = "Death")
+
