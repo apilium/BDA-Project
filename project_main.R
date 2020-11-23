5b0b7f7d273604b9fa4591a331f2b195482c01b4
Nicola Brazzale <brazzalenicola@gmail.com>
2020-11-23 10:11:14 +0200

    correlation matrix and suggestions for next points

--- project_main.R
+++ project_main.R
@@ -1,4 +1,6 @@
 library(ggplot2)
+library(bayesplot)
+library(corrplot)
 
 heart <- read.csv(file = 'heart_failure_clinical_records_dataset.csv')
 head(heart)
@@ -15,3 +17,18 @@ ggplot(heart, aes(x=platelets)) + geom_histogram(aes(fill=as.character(DEATH_EVE
 
 ggplot(heart, aes(x=serum_creatinine)) + geom_histogram(aes(fill=as.character(DEATH_EVENT)), bins = 30) + labs(fill = "Death")
 
+
+pred <- c("high_blood_pressure", "age", "sex", "creatinine_phosphokinase", "diabetes", "ejection_fraction", "platelets", "serum_creatinine", "serum_sodium", "smoking", "anaemia") 
+target <- c("DEATH_EVENT")
+#formula <- paste("DEATH_EVENT ~", paste(pred, collapse = "+"))
+p <- length(pred)
+n <- nrow(heart)
+x = cor(heart[, c(target,pred)]) 
+corrplot(x)
+
+#PRIORS ?
+# 1 LINEAR MODEL WITH VARIABLE SELECTION
+# 2 LINEAR MODEL WITH ALL VARIABLES
+# HIERARCHICAL - in the Titanic one a hier. model have been used so we can use that one for reference
+
+# All models with bernoulli outcome (1-0, death or not)
