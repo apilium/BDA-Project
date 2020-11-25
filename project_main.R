07f3bd9c14657536fcf08d724107a59ea6a0c3a1
Nicola Brazzale <brazzalenicola@gmail.com>
2020-11-25 15:41:30 +0200

    model with 4 variables

--- project_main.R
+++ project_main.R
@@ -1,8 +1,8 @@
 library(ggplot2)
 library(bayesplot)
 library(corrplot)
-
-heart <- read.csv(file = 'heart_failure_clinical_records_dataset.csv')
+library(brms)
+heart <- read.csv(file = 'data/heart_failure_clinical_records_dataset.csv')
 head(heart)
 
 ggplot(heart, aes(x=age)) + geom_histogram(aes(fill=as.character(sex)), bins = 30) + labs(fill = "Sex")
@@ -32,3 +32,33 @@ corrplot(x)
 # HIERARCHICAL - in the Titanic one a hier. model have been used so we can use that one for reference
 
 # All models with bernoulli outcome (1-0, death or not)
+test.size <- 0.3
+train.indice <- sample(nrow(heart), nrow(heart)*(1-test.size))
+train.data <- heart[train.indice,]
+test.data <- heart[-train.indice,]
+
+fit <- brm(formula = DEATH_EVENT ~ age + ejection_fraction + serum_creatinine + serum_sodium,
+           data = train.data,
+           family = bernoulli(),
+           prior = set_prior('normal(0, 1000)'),
+           refresh=0
+)
+
+preds <- round(predict(fit, newdata = test.data))[1]
+pred.corr <- preds == test.data$DEATH_EVENT
+
+acc <- length(pred.corr[pred.corr == TRUE])/nrow(test.data)
+acc
+
+fit1 <- brm(formula = DEATH_EVENT ~ age + ejection_fraction + serum_creatinine + serum_sodium + high_blood_pressure + creatinine_phosphokinase + diabetes + smoking + anaemia,
+           data = train.data,
+           family = bernoulli(),
+           prior = set_prior('normal(0, 1000)'),
+           refresh=0
+)
+
+preds1 <- round(predict(fit1, newdata = test.data))[1]
+pred1.corr <- preds1 == test.data$DEATH_EVENT
+
+acc <- length(pred1.corr[pred1.corr == TRUE])/nrow(test.data)
+acc
